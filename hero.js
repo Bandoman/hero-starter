f1388b5dcade7a23113e445a55516ff1895c23a3
Bandoman <w.r.vos1990@gmail.com>
2014-10-15 12:40:35 +0200

    Update hero.js

--- hero.js
+++ hero.js
@@ -92,16 +92,14 @@ var move = function(gameData, helpers) {
   var distanceToHealthWell = healthWellStats.distance;
   var directionToHealthWell = healthWellStats.direction;
   
-
   if (myHero.health < 40) {
-    //Heal no matter what if low health
     return directionToHealthWell;
   } else if (myHero.health < 100 && distanceToHealthWell === 1) {
-    //Heal if you aren't full health and are close to a health well already
     return directionToHealthWell;
+  } else if (myHero.health < 60) {     
+    return helpers.findNearestHealthWell(gameData);
   } else {
-    //If healthy, go capture a diamond mine!
-    return helpers.findNearestNonTeamDiamondMine(gameData);
+    return helpers.findNearestTeamMember(gameData);
   }
 };
 
